39dda541124fe9e68701f0d77266d119acfe8ec0
nbigaud <nathan.bigaud@dalberg.com>
2020-12-03 06:41:34 +0000

    First working test version

--- coc-dashboard/components/datacard_5.py
+++ coc-dashboard/components/datacard_5.py
@@ -1,5 +1,6 @@
 from store import (timeit,
                    reporting_count_transform,
+                   reporting_count_transform_tooltip,
                    init_data_set,
                    get_report_perc,
                    DEFAULTS,
@@ -17,14 +18,23 @@ def bar_reporting_country_plot(data):
     return data_out
 
 
+@timeit
+def bar_reporting_country_plot_tooltip(data):
+
+    data_in = data.get("reporting_country")
+    data_out = reporting_count_transform_tooltip(data_in.copy())
+
+    return data_out
+
+
 def get_title_reporting_country(data, indicator_view_name, **controls):
     """
     get title for the reporting section based on a percentage calculation and the inputs
     """
     descrip_reported, descrip_positive = get_report_perc(data, **controls)
 
-    title = f'''Reporting: on {controls.get('target_month')}-{controls.get('target_year')}, 
-            {descrip_reported} of facilities reported on their 105:1 form, and, out of those, 
+    title = f'''Reporting: on {controls.get('target_month')}-{controls.get('target_year')},
+            {descrip_reported} of facilities reported on their 105:1 form, and, out of those,
             {descrip_positive} reported one or above for {indicator_view_name}'''
 
     return title
@@ -39,12 +49,18 @@ default_title = get_title_reporting_country(bar_reporting_country_plot(init_data
                                                 DEFAULTS.get('indicator')),
                                             **DEFAULTS)
 
-stacked_bar_reporting_country = ChartDataCard(
+customdata = [bar_reporting_country_plot_tooltip(init_data_set).get('Reported one or above for selected indicator').iloc[:, -1:],
+
+
+stacked_bar_reporting_country= ChartDataCard(
     data=init_data_set,
     data_transform=bar_reporting_country_plot,
     title=default_title,
     fig_title="$label$",
     fig_object="Scatter",
+    trace_params={
+        'customdata':,
+        'hovertemplate': 'test:%{customdata:.3f}'},
 )
 
 # stacked_bar_reporting_country.set_colors(
